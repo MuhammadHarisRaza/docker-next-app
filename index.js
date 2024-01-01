1b8043fdbcbfc5e3389dc82bca2414d7ddb8084c
MuhammadHarisRaza <razaharis132@gmail.com>
2024-01-01 07:45:43 +0500

    modules deleted

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const app = express();
 app.use(express.json());
 
 //cors
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Origin", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
 
 //test api
 
